b5dc322d01119132f36059fb893c7ce10430ea80
Max Genash <max_genash@mail.ru>
2016-03-15 00:02:47 +0200

    wrote initial scaffolding of frontend

---
+++ frontend/client/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//import App from './components/App.jsx';
+import './common_assets/styles/style.less';
+
+/*ReactDOM.render(
+    <App />,
+    document.getElementById('mount-point')
+);
+*/
---
+++ frontend/webpack.config.js
@@ -0,0 +1,75 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: "./client/main.jsx",
+    output: {
+        path: __dirname + '/public/build/',
+        publicPath: "build/",
+        filename: "bundle.js"
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+    devtool: "eval-source-map",
+    module: {
+        preLoaders: [
+            {
+                test: /\.jsx$/,
+                loader: "source-map",
+                exclude: [/node_modules/, /public/]
+            }
+        ],
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel?presets[]=es2015",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader!autoprefixer-loader",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.less$/,
+                loader: "style-loader!css-loader!autoprefixer-loader!less",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.gif$/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            },
+            {
+                test: /\.svg/,
+                loader: "url-loader?limit=26000&mimetype=image/svg+xml"
+            },
+            {
+                test: /\.jsx$/,
+                loader: "react-hot!babel?presets[]=es2015,presets[]=react",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test:   /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+                loader: 'file?name=[path][name].[ext]?[hash]'
+            },
+            {
+                test: /\.json$/,
+                loader: "json-loader"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        })
+    ]
+};
